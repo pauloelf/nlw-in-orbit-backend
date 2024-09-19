1241d65a17ccd78c6363ceb8baa3750d9b3c988f
Paulo Sergio <contato.paulofws@gmail.com>
2024-09-19 11:25:12 -0300

    add: add querys and pending-goals route

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,17 +2,25 @@ import fastify from 'fastify'
 import { createGoal } from '../functions/create-goal'
 import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+  return {
+    pendingGoals
+  }
+})
+
 app.post('/goals', {
   schema: {
     body: z.object({
       title: z.string(),
-      desiredWeeklyFrequency: z.number().int().min(1).max(7)    
+      desiredWeeklyFrequency: z.number().int().min(1).max(7) 
     })
   }
 }, async request => {
