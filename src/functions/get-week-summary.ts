3ba8140a66d06cc0bf443f9510de2d260f4ee53d
Paulo Sergio <contato.paulofws@gmail.com>
2024-11-07 12:14:13 -0300

    feat: sorting goals

--- src/functions/get-week-summary.ts
+++ src/functions/get-week-summary.ts
@@ -1,4 +1,4 @@
-import { and, eq, gte, lte, sql } from "drizzle-orm"
+import { and, desc, eq, gte, lte, sql } from "drizzle-orm"
 import { db } from "../db"
 import { goalCompletions, goals } from "../db/schema"
 import dayjs from "dayjs"
@@ -38,6 +38,7 @@ export async function getWeekSummary() {
       gte(goalCompletions.createdAt, firstDayOfWeek),
       lte(goalCompletions.createdAt, lastDayOfWeek)
     ))
+    .orderBy(desc(goalCompletions.createdAt))
   )
 
   const goalsCompletedByWeekDay = db.$with('goals_completed_by_week_day').as(
@@ -53,7 +54,9 @@ export async function getWeekSummary() {
         )
       `.as('completions'),
     })
-    .from(goalsCompletedInWeek).groupBy(goalsCompletedInWeek.completedAtDate)
+    .from(goalsCompletedInWeek).
+    groupBy(goalsCompletedInWeek.completedAtDate)
+    .orderBy(desc(goalsCompletedInWeek.completedAtDate))
   )
 
   const result = await db
